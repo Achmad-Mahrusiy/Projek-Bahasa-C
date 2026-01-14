32a55e2740a46842ef7a44805d1bd0e0ae647384
troii <lolitunbina@gmail.com>
2026-01-14 15:02:51 +0700

    logic

---
+++ C-Sharp/logical-operator.cs
@@ -0,0 +1,33 @@
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter yout age: ");
+            double age = Convert.ToDouble(Console.ReadLine());
+
+
+            if (age >= 0 && age <= 6)
+            {
+                Console.WriteLine("Nikmati masa kecilmu");
+            }
+
+            else if (age >= 7 && age <= 15)
+            {
+                Console.WriteLine("Maksimalkan potensimu, karena kamu masih muda");
+            }
+
+            else if (age >= 16 && age <= 24)
+            {
+                Console.WriteLine("Maksimalkan potensi remajamu");
+            }
+
+            else
+            {
+                Console.WriteLine("Kembangkan pekerjaanmu");
+            }
+        }
+        Console.ReadKey();
+      }
+  }
