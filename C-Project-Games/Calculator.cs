2ae8cdbbb02c74897c8609d9e5035b395f4c0747
troii <lolitunbina@gmail.com>
2026-01-18 10:52:12 +0700

    menghitung

--- C-Project-Games/Calculator.cs
+++ C-Project-Games/Calculator.cs
@@ -10,6 +10,7 @@ namespace MyFirstProgram
             {
                 double num1 = 0;
                 double num2 = 0;
+                double num3 = 0;
                 double result = 0;
 
                 Console.WriteLine("------------------");
@@ -22,30 +23,33 @@ namespace MyFirstProgram
                 Console.Write("Masukkan angka kedua: ");
                 num2 = Convert.ToDouble(Console.ReadLine());
 
+                Console.Write("Masukkan angka ketiga: ");
+                num3 = Convert.ToDouble(Console.ReadLine());
+
                 Console.WriteLine("Masukkan Opsi: ");
-                Console.WriteLine("\t+ : tambah");
-                Console.WriteLine("\t- : kurang");
-                Console.WriteLine("\t* : kali");
-                Console.WriteLine("\t/ : bagi");
+                Console.WriteLine("\t1. + : tambah");
+                Console.WriteLine("\t2. - : kurang"); 
+                Console.WriteLine("\t3. * : kali");
+                Console.WriteLine("\t4.  / : bagi");
                 Console.Write("Masukkan opsi: ");
 
                 switch (Console.ReadLine())
                 {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine($"Hasilnya : {num1} + {num2} = " + result);
+                    case "1":
+                        result = num1 + num2 + num3;
+                        Console.WriteLine($"Hasilnya : {num1} + {num2} + {num3} = " + result);
                         break;
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"Hasilnya : {num1} - {num2} = " + result);
+                    case "2":
+                        result = num1 - num2 - num3;
+                        Console.WriteLine($"Hasilnya : {num1} - {num2} - {num3} = " + result);
                         break;
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"Hasilnya : {num1} * {num2} = " + result);
+                    case "3":
+                        result = num1 * num2 * num3;
+                        Console.WriteLine($"Hasilnya : {num1} * {num2} * {num3}= " + result);
                         break;
-                    case "/":
-                        result = num1 / num2;
-                        Console.WriteLine($"Hasilnya : {num1} / {num2} = " + result);
+                    case "4":
+                        result = num1 / num2 / num3;
+                        Console.WriteLine($"Hasilnya : {num1} / {num2} / {num3}= " + result);
                         break;
                     default:
                         Console.WriteLine("Opsi tidak valid");
